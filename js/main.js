f44b4e3e4f7d57d81be61d34692b560617441afd
towhidulislam2023 <towhidulislam2.bd@gmail.com>
2023-03-03 04:31:57 +0600

    sortBydate btn

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
-console.log("connected");
+// sortBydate btn 
+document.getElementById('sortBydate').addEventListener("click", function () {
+    const URL = `https://openapi.programming-hero.com/api/ai/tools`
+    fetch(URL).then(res => res.json()).then(data => dosort(data.data.tools))
 
+    dosort = (data) => {
+        console.log(data);
+        datesort = (a, b) => {
+            const dateA =Number(new Date(a.published_in));
+            const dateB =Number(new Date(b.published_in));
+            if (dateA < dateB) {
+                return 1;
+                
+            }
+            else if (dateB < dateA) {
+                return -1
+            }
+            else {
+                return 0
+            }
+        }
+        displayCardData(data.sort(datesort));
+        console.log(data.sort(datesort));
+    }
+})
 
+///load data
 const loadAllData = (inputNum) => {
     const URL = `https://openapi.programming-hero.com/api/ai/tools`
     fetch(URL).then(res => res.json()).then(data => {
@@ -10,29 +34,22 @@ const loadAllData = (inputNum) => {
 }
 
 const displayCardData = (data) => {
+
     console.log(data.length);
     if (data.length === 6) {
         document.getElementById('showAllBtn').classList.remove('d-none')
-        
+
     }
     else {
         document.getElementById('showAllBtn').classList.add('d-none')
-        
+
     }
 
     const targetContainer = document.getElementById('cardContainer')
     targetContainer.innerHTML = '';
-    // console.log(data);
-
 
     data.forEach(element => {
-        console.log(element);
-
-
-
-
-
-
+        // console.log(element.published_in);
         targetContainer.innerHTML += `
         <div class="col">
         <div class="card h-100 p-1">
@@ -41,10 +58,10 @@ const displayCardData = (data) => {
                 <h3 class="card-title fw-bold">Features</h3>
                 <div class="card-text">
                     <ol>
-                        <li>${element.features[0]?element.features[0]:""}</li>
-                        <li>${element.features[1]?element.features[1]:""}</li>
-                        <li>${element.features[2]?element.features[2]:" Not Available"}</li>
-                        <li style="list-style:none;">${element.features[3]?element.features[3]:""}</li>
+                        <li>${element.features[0] ? element.features[0] : ""}</li>
+                        <li>${element.features[1] ? element.features[1] : ""}</li>
+                        <li>${element.features[2] ? element.features[2] : " Not Available"}</li>
+                        <li style="list-style:none;">${element.features[3] ? element.features[3] : ""}</li>
                     </ol>
                 </div>
             </div>
@@ -65,56 +82,48 @@ const displayCardData = (data) => {
 `
     });
     loadSpinner(false)
+
 }
-
-
 //modal Data
-
 const loadModalData = (id) => {
     const URL = `https://openapi.programming-hero.com/api/ai/tool/${id}`
     console.log(URL);
     fetch(URL).then(res => res.json()).then(data => {
         displayModalData(data.data);
     })
-    
 }
 
 const displayModalData = (data) => {
-    // console.log(data.pricing ? data.pricing[2].price:" ");
- console.log(data.accuracy.score*100);
-
-    
+    console.log(data.accuracy.score * 100);
 
 
 
-    let planNameforbasic =data.pricing?data.pricing[0].plan:"Free of Cost/Basic";
-    if (planNameforbasic === 'Free'||planNameforbasic === '0') {
-        planNameforbasic='Basic'
-        
+
+
+    let planNameforbasic = data.pricing ? data.pricing[0].plan : "Free of Cost/Basic";
+    if (planNameforbasic === 'Free' || planNameforbasic === '0') {
+        planNameforbasic = 'Basic'
+
     }
-    let priceingFeeforbasic = (data.pricing?data.pricing[0].price:" ");
-    if (priceingFeeforbasic === '0'||priceingFeeforbasic === 'No cost') {
-        priceingFeeforbasic="Free Of Cost/"
+    let priceingFeeforbasic = (data.pricing ? data.pricing[0].price : " ");
+    if (priceingFeeforbasic === '0' || priceingFeeforbasic === 'No cost') {
+        priceingFeeforbasic = "Free Of Cost/"
     }
-    let planNameforpro =data.pricing? data.pricing[1].plan:"Free of Cost/pro";
-    if (planNameforpro === 'Free'||planNameforpro === '0') {
-        planNameforpro='pro'
-        
+    let planNameforpro = data.pricing ? data.pricing[1].plan : "Free of Cost/pro";
+    if (planNameforpro === 'Free' || planNameforpro === '0') {
+        planNameforpro = 'pro'
+
     }
-    let priceingFeeforpro = (data.pricing?data.pricing[1].price:"");
-    if (priceingFeeforpro === '0'||priceingFeeforpro === 'No cost') {
-        priceingFeeforpro="Free Of Cost/"
+    let priceingFeeforpro = (data.pricing ? data.pricing[1].price : "");
+    if (priceingFeeforpro === '0' || priceingFeeforpro === 'No cost') {
+        priceingFeeforpro = "Free Of Cost/"
     }
 
-    // data.forEach(elem => {
-    //     console.log(elem);
-        
-    // });
-    let accuracyData = data.accuracy.score * 100 +"% accuracy";
+    let accuracyData = data.accuracy.score * 100 + "% accuracy";
     if (data.accuracy.score * 100 === 0) {
-        accuracyData=''
-    
-}
+        accuracyData = ''
+
+    }
 
 
 
@@ -152,7 +161,7 @@ const displayModalData = (data) => {
                             <div class="bg-light rounded-2 d-flex justify-content-center align-items-center"
                                 style="height: 100px; width: 150px;">
                                 <p class="text-center fw-bold text-danger w-100">
-                                ${data.pricing ? data.pricing[2].price : " Free of Cost /Enterprise"} ${data.pricing ? data.pricing[2].plan:" "}
+                                ${data.pricing ? data.pricing[2].price : " Free of Cost /Enterprise"} ${data.pricing ? data.pricing[2].plan : " "}
                                 </p>
 
                             </div>
@@ -178,13 +187,13 @@ const displayModalData = (data) => {
                                     <h5 class="fw-bold">Integrations</h5>
                                     <ul>
                                         <li>
-                                           ${data.integrations?data.integrations[0]?data.integrations[0]:"No Data Found":"No Data Found"}
+                                           ${data.integrations ? data.integrations[0] ? data.integrations[0] : "No Data Found" : "No Data Found"}
                                         </li>
                                         <li>
-                                           ${data.integrations?data.integrations[1]?data.integrations[1]:"No Data Found":"No Data Found"}
+                                           ${data.integrations ? data.integrations[1] ? data.integrations[1] : "No Data Found" : "No Data Found"}
                                         </li>
                                         <li>
-                                        ${data.integrations?data.integrations[2]?data.integrations[2]:"No Data Found":"No Data Found"}
+                                        ${data.integrations ? data.integrations[2] ? data.integrations[2] : "No Data Found" : "No Data Found"}
                                         </li>
                                     </ul>
 
@@ -203,12 +212,12 @@ const displayModalData = (data) => {
                 <div style="background-color: rgba(223, 1, 129); top:15px; left:24px;" class=" position-absolute px-3 rounded fw-bold text-light">${accuracyData}</div>
                     <img src="${data.image_link[0]}" class="card-img-top" alt="...">
                     <div class="card-body"><h5 class="card-text fw-bold text-center">
-                    ${data.input_output_examples?data.input_output_examples[0].input :"Can you give any example?"}</h5>
-                        <p class="text-center">${data.input_output_examples?data.input_output_examples[1].output: "No! Not Yet! Take a break!!!"}</p>
+                    ${data.input_output_examples ? data.input_output_examples[0].input : "Can you give any example?"}</h5>
+                        <p class="text-center">${data.input_output_examples ? data.input_output_examples[1].output : "No! Not Yet! Take a break!!!"}</p>
                         <br>
                         <h5 class="card-text fw-bold text-center">
-                        ${data.input_output_examples?data.input_output_examples[1].input: " "}</h5>
-                        <p class="text-center">${data.input_output_examples?data.input_output_examples[1].output: ""}</p>
+                        ${data.input_output_examples ? data.input_output_examples[1].input : " "}</h5>
+                        <p class="text-center">${data.input_output_examples ? data.input_output_examples[1].output : ""}</p>
                     </div>
                 </div>
             </div>
@@ -227,24 +236,17 @@ const displayModalData = (data) => {
     
     
     `
-
-    
 }
-
 function loadSpinner(isTrue) {
     if (isTrue === true) {
         document.getElementById('spinner').classList.remove('d-none')
-        
+
     }
     else {
         document.getElementById('spinner').classList.add('d-none')
-        
+
     }
-    
+
 }
 
-
-
 loadAllData(6);
-
-
